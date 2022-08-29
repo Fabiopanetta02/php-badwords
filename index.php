4313b527651aa15b40d56af4d4dcd61d350ad2bd
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-08-29 13:54:49 +0200

    Added badwords and printing paragraph and lenght

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 $paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi architecto, harum, debitis aliquid unde laudantium quia molestias quo tempora molestiae similique dignissimos ipsum rerum adipisci hic nisi omnis aut rem.';
-$lenght_string = strlen($paragraph)
+$lenght_paragraph = strlen($paragraph);
+
+$bad_words = $_GET['badwords'];
+
+$replace_paragraph = str_replace($bad_words, '***', $paragraph);
+$lenght_replace_paragraph = strlen($replace_paragraph);
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +18,10 @@ $lenght_string = strlen($paragraph)
 </head>
 <body>
     <p> <?= $paragraph?> </p>
-    <p> <?= $lenght_string?> </p>
+    <p> <?= $lenght_paragraph?> </p>
+
+    <p> <?= $replace_paragraph?> </p>
+    <p> <?= $lenght_replace_paragraph?> </p>
+
 </body>
 </html>
